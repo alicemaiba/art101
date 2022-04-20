d302cd198025c952942c9fe1444f657b5b1c1973
Alice <aaiba@ucsc.edu>
2022-04-19 16:40:19 -0800

    saving

---
+++ lab5/js/lab.js
@@ -0,0 +1,22 @@
+// Summary: Lab 5 - Data Types and Variables
+// Description: Experiment with basic JavaScript data types and variables.
+// @author Alejandra Sicairos and Alice Aiba
+// @created  4.19.22
+
+//REMEMBER TO PUT A SEMICOLON AT THE END OF EVERY LINE!
+
+//Declare variables: make, model, year, and color
+make = "Honda"
+model = "Odyssey"
+year = "2013"
+color = "tan"
+
+//Calculate the age
+age = 2022 - year;
+
+//Print everything
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Age: " + age + " Years<br>");
