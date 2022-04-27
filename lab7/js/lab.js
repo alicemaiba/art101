e3b9184027dc52d23e8313030759f2e75418eb66
Alice <aaiba@ucsc.edu>
2022-04-26 17:17:22 -0800

    save

--- lab7/js/lab.js
+++ lab7/js/lab.js
@@ -1,1 +1,22 @@
+// Summary: Lab 7: Functions
+// Description: Experiment with basic JavaScript data types and variables.
+// @author Alejandra Sicairos and Alice Aiba
+// @created  4.26.22
 
+
+
+function sortString(){
+   var userName = window.prompt("Hi. Please tell me your name so I can sort it.");
+   // convert Str to array
+   var array = userName.split('');
+   // Reverse String
+   array.sort();
+   // convert array back to string
+   var newStr = array.join('');
+   // return the new str
+   return newStr;
+}
+
+ //output
+ document.writeIn("Oh hey, I've sorted your name:",
+sortString(userName),"</br>");
