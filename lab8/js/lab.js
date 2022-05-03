b541f468b274612d72e1ba4064d79359f9396b72
Alice <aaiba@ucsc.edu>
2022-05-02 17:25:43 -0800

    save

--- lab8/js/lab.js
+++ lab8/js/lab.js
@@ -4,6 +4,8 @@
 *Liscense: Public Domain
 */
 
+//@param: int/number
+//@output: true or false
 function isEven(x){
   return (x % 2 == 0);
 }
@@ -12,12 +14,12 @@ function isEven(x){
 console.log("Is 1 even?", isEven(1));
 console.log("Is 2 even?", isEven(2));
 
-array = [35, 888, 2510, 7, 202201, 300, 99]
+var array = [35, 888, 2510, 7, 202201, 300, 99];
 console.log("My Array", array);
 
-var result = array.map(isEven);
-// should return [false, true, true, false, false, true, false]
-console.log("Test for evenness of array:", result);
+var evenResult = array.map(isEven);
+// should return [false, true, true, false, false, true, false]
+console.log("Test for evenness of array:", evenResult);
 
 var result = array.map(function(x){
     return x / 5;
