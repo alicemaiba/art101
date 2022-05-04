5031aac7c93881bcb0f85cf1ccabda8cd666d898
Alice <aaiba@ucsc.edu>
2022-05-03 16:44:35 -0800

    save

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -2,3 +2,17 @@
 // Description: Experiment with DOM Manipulation
 // @author Alejandra Sicairos and Alice Aiba
 // @created  5.3.22
+
+// Use getElementById() to find your output <div> and assign it to a variable outputEl
+var outputEl = document.getElementById("output");
+// Create a new element with document.createElement("p") and assign it to a variable new1El
+var new1El = document.createElement("p");
+// Change the html attribute of new1El to say something new.
+new1El.innerHTML = "";
+// Create another new element and assign it to a variable new2El
+var new2El = document.createElement("p");
+// Change the html attribute of new2El to say something else.
+new2El.innerHTML = "";
+// Append both new elements one at a time using appendChild() to your output <div>
+outputEl.appendChild(new1El);
+outputEl.appendChild(new2El);
