8d67482b02e35978201613d8b8470e27a1dfc787
Alice <aaiba@ucsc.edu>
2022-05-10 17:46:44 -0800

    save

--- lab11/js/lab.js
+++ lab11/js/lab.js
@@ -1,1 +1,19 @@
+// Summary: Lab 11: Libraries & jQuery
+// Description: Experiment with jQuery
+// @author Alice Aiba and Alejandra Sicairos
+// @created 5.10.22
+var colors = ["red", "blue", "yellow", "green", "purple", "#c45672", "brown"];
+//var random_color = colors[Math.floor(Math.random() * colors.length)];
 
+$("#context p").append("<button>Press");
+
+$("button").click(function(){
+  //$("#content p").toggleClass("selected")
+  var random_color = colors[Math.floor(Math.random() * colors.length)];
+  $("#content p").css('color', random_color);
+});
+
+$("button").click(function(){
+  var random_color = colors[Math.floor(Math.random() * colors.length)];
+  $("#button").css('color', random_color);
+});
