9df5ab19969d36f51795121939ee71111e6df336
Alice <aaiba@ucsc.edu>
2022-05-18 18:13:18 -0800

    save

--- lab13/js/lab.js
+++ lab13/js/lab.js
@@ -3,30 +3,37 @@
 // @author Alejandra Sicairos and Alice Aiba
 // @created  5.17.22
 
-//print num 1-100
-for (var i = 1; i <= 100; i++){
-  var str = '';
-  //if divisable by 2 print fizz
-  if (i % 2 == 0){
-    str += "Fizz";
+//print num 1-any number selected
+function FizzBuzz(input){
+  for (var i = 1; i <= input; i++){
+    var str = i + '. ';
+
+    //if divisable by 3 print fizz
+    if (i % 2 == 0){
+      str += "Fizz";
+    }
+    //if divisable by 5 print buzz
+    if(i % 5 == 0){
+      str += "Buzz";
+    }
+    // divisable by
+    if (i % 9 == 0){
+      str += "Bang";
+    }
+    // if (str == ''){
+    //   console.log(i);
+    // }else{
+    //   //if divisable by 3 and 5 bring both fizz and buzz
+    //   console.log(str);
+    // }
+    $("#output").append("<p>" + str +"</p>");
   }
-  //if divisable by 5 print buzz
-  if(i % 5 == 0){
-    str += "Buzz";
-  }
-  //if divisible by 9 print Bang
-  if(i % 9 == 0){
-    str += "Bang";
-  }
-  if (str == ''){
-    console.log(i);
-  }else{
-    //if divisable by 3 and 5 bring both fizz and buzz
-    console.log(str);
-  }
-  $("#output").append("<p>" + str +"</p>");)
 }
 
-$("button").click(function(){
-  var output = FizzBuzz ();
+$("#button").click(function(){
+  //Clears output so that everytime button is clicked its the new number and doesnt add to it
+  $("#output").empty();
+  //input that lets users choose their amount of numbers
+  var input = $("#input").val();
+  var output = FizzBuzz(input);
 })
