9b0fbf813378a60d65edb7fa6b71367cdaa67bbb
Alice <aaiba@ucsc.edu>
2022-05-23 20:20:37 -0800

    new js

--- lab14/js/lab.js
+++ lab14/js/lab.js
@@ -1,31 +1,28 @@
-// Summary: Lab 13: Loops
+// Summary: Lab 14: Debug
 // Description: Experiment with loops.
 // @author Alejandra Sicairos and Alice Aiba
-// @created  5.17.22
+// @created  5.23.22
 
-//print num 1-any number selected
-function FizzBuzz(input){
-  for (var i = 1; i <= input; i++){
-    var str = i + '. ';
+//print num 1-any number selected, and random words inputted
+function FizzBuzz(max, a, b, c){
+  for (var i = 1; i <= max; i++){
+    var str = i + ' ';
 
     //if divisable by 3 print fizz
     if (i % 2 == 0){
-      str += "Fizz";
+      str += a;
     }
     //if divisable by 5 print buzz
     if(i % 5 == 0){
-      str += "Buzz";
+      str += b;
     }
     // divisable by
     if (i % 9 == 0){
-      str += "Bang";
+      str += c;
     }
-    // if (str == ''){
-    //   console.log(i);
-    // }else{
-    //   //if divisable by 3 and 5 bring both fizz and buzz
-    //   console.log(str);
-    // }
+    //make everything uppercase
+    str = str.toUpperCase();
+    str += "!";
     $("#output").append("<p>" + str +"</p>");
   }
 }
@@ -34,6 +31,10 @@ $("#button").click(function(){
   //Clears output so that everytime button is clicked its the new number and doesnt add to it
   $("#output").empty();
   //input that lets users choose their amount of numbers
-  var input = $("#input").val();
-  var output = FizzBuzz(input);
+  var max = $("#max").val();
+  var text1 = $("#text1").val();
+  var text2 = $("#text2").val();
+  var text3 = $("#text3").val();
+
+  var output = FizzBuzz(max, text1, text2, text3);
 })
